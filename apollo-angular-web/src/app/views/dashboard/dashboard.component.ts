3a946fad759784a4c2c2d8072459c2f9338beb62
GathoniWainaina <cynthia.wainaina@bskglobaltech.com>
2021-06-23 10:25:15 +0300

    breadcrumbs

--- apollo-angular-web/src/app/views/dashboard/dashboard.component.ts
+++ apollo-angular-web/src/app/views/dashboard/dashboard.component.ts
@@ -20,4 +20,26 @@ export class DashboardComponent implements OnInit {
     this.fullName$ = this.store$.pipe(select(selectGetNames));
   }
 
+  public getBreadcrumbs(): string {
+    const here = location.href.split('/').slice(3);
+    // console.log(here)
+    const parts = [{"text": 'Home', "link": '/'}];
+    // console.log(parts)
+
+    for (let i = 0; i < here.length; i++) {
+      const part = here[i];
+      // console.log(part)
+      const text = decodeURIComponent(part).split('.')[0];
+      // console.log(text)
+      const link = '/' + here.slice(0, i + 1).join('/');
+      console.log(link)
+      parts.push({"text": text, "link": link});
+      // console.log(parts)
+    }
+    return parts.map((part) => {
+      const breadCrumb = "<a href=\"" + part.link + "\" class='bredcrumb'>" + part.text + "</a>"
+      return breadCrumb
+    }).join('/')
+  }
+
 }
