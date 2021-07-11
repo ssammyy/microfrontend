bbbdd4f57997a642e7952ff09971c0b1e4f59d3b
Marvin Makau <mbiko.mb@gmail.com>
2021-07-11 06:32:18 +0300

    changes to login

--- apollo-angular-newui/src/app/app.module.ts
+++ apollo-angular-newui/src/app/app.module.ts
@@ -111,7 +111,7 @@ import {DmarkApplicationsAllComponent} from "./apollowebs/dmark-applications-all
         EntityDataModule.forRoot(entityConfig),
         ReactiveFormsModule,
         RouterModule,
-        CoreModule
+        // CoreModule
     ],
     declarations: [
         LoginComponent,
