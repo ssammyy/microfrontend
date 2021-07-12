63133ac9bb5f6c09a7ec0d330fa7ca4c3a1fdaaf
Kalenda Paul <kpaul7747@gmail.com>
2021-07-12 20:22:02 +0300

    QA STATUS smark details details
    Signed-off-by: Kalenda Paul <kpaul7747@gmail.com>

--- apollo-angular-newui/src/app/apollowebs/new-smark-permit/new-smark-permit.component.ts
+++ apollo-angular-newui/src/app/apollowebs/new-smark-permit/new-smark-permit.component.ts
@@ -15,6 +15,9 @@ import swal from "sweetalert2";
 export class NewSmarkPermitComponent implements OnInit {
   sta1Form: FormGroup;
   sta10Form: FormGroup;
+    sta10FormA: FormGroup;
+    sta10FormB: FormGroup;
+    sta10FormC: FormGroup;
   sections: SectionDto[];
   plants: PlantDetailsDto[];
   permitEntityDetails: PermitEntityDetails;
@@ -39,14 +42,51 @@ export class NewSmarkPermitComponent implements OnInit {
 
         });
       this.sta10Form = this.formBuilder.group({
-          produceOrdersOrStock: ['', Validators.required],
-          issueWorkOrderOrEquivalent: ['', Validators.required],
-          identifyBatchAsSeparate: ['', Validators.required],
-          productsContainersCarryWorksOrder: ['', Validators.required],
-          isolatedCaseDoubtfulQuality: ['', Validators.required]
+          firmName: ['', Validators.required],
+          statusCompanyBusinessRegistration: ['', Validators.required],
+          ownerNameProprietorDirector: ['', Validators.required],
+          postalAddress: ['', Validators.required],
+          contactPerson: ['', Validators.required],
+          telephone: ['', Validators.required],
+          emailAddress: ['', Validators.required],
+          physicalLocationMap: ['', Validators.required],
+          county: ['', Validators.required],
+          town: ['', Validators.required],
+          totalNumberFemale: ['', Validators.required],
+          totalNumberMale: ['', Validators.required],
+          totalNumberPermanentEmployees: ['', Validators.required],
+          totalNumberCasualEmployees: ['', Validators.required]
 
       });
+      this.sta10FormA = this.formBuilder.group({
+          personnelName: ['', Validators.required],
+          qualificationInstitution: ['', Validators.required],
+          dateOfEmployment: ['', Validators.required]
 
+      });
+      this.sta10FormB = this.formBuilder.group({
+          productName: ['', Validators.required],
+          productBrand: ['', Validators.required],
+          productStandardNumber: ['', Validators.required],
+          available: ['', Validators.required],
+          permitNo: ['', Validators.required],
+          name: ['', Validators.required],
+          origin: ['', Validators.required],
+          specifications: ['', Validators.required],
+          qualityChecksTestingRecords: ['', Validators.required],
+          machineName: ['', Validators.required],
+          typeModel: ['', Validators.required],
+          countryOfOrigin: ['', Validators.required],
+          processFlowOfProduction: ['', Validators.required],
+          operations: ['', Validators.required],
+          criticalProcessParametersMonitored: ['', Validators.required],
+          frequency: ['', Validators.required],
+          processMonitoringRecords: ['', Validators.required]
+
+      });
+      this.sta10FormC = this.formBuilder.group({
+
+      });
 
     this.qaService.loadSectionList().subscribe(
         (data: any) => {
@@ -77,12 +117,15 @@ export class NewSmarkPermitComponent implements OnInit {
                     this.stepSoFar = {...this.sta1Form?.value};
                     break;
                 case 2:
-                    this.stepSoFar = {...this.sta1Form?.value};
+                    this.stepSoFar = {...this.sta10Form?.value};
                     break;
                 case 3:
-                    this.stepSoFar = {...this.sta1Form?.value};
+                    this.stepSoFar = {...this.sta10FormA?.value};
                     break;
                 case 4:
+                    this.stepSoFar = {...this.sta10FormB?.value};
+                    break;
+                case 5:
                     this.stepSoFar = {...this.sta1Form?.value};
                     break;
             }
@@ -129,6 +172,8 @@ export class NewSmarkPermitComponent implements OnInit {
             return 'activated';
         }if (step === 4) {
             return 'activated';
+        }if (step === 5) {
+            return 'activated';
         } else {
             return '';
         }
@@ -137,7 +182,18 @@ export class NewSmarkPermitComponent implements OnInit {
   get formSta1Form(): any {
     return this.sta1Form.controls;
   }
-
+    get formSta10Form(): any {
+        return this.sta10Form.controls;
+    }
+    get formSta10FormA(): any {
+        return this.sta10FormA.controls;
+    }
+    get formSta10FormB(): any {
+        return this.sta10FormB.controls;
+    }
+    get formSta10FormC(): any {
+        return this.sta10FormC.controls;
+    }
   onClickSaveSTA1(valid: boolean) {
     if (valid) {
       if (this.permitEntityDetails == null) {
@@ -177,6 +233,26 @@ export class NewSmarkPermitComponent implements OnInit {
       }
     }
   }
+    onClickSaveSTA10(valid: boolean) {
+        if (valid) {
+            console.log(this.permitEntityDetails.id.toString());
+            this.qaService.savePermitSTA3(this.permitEntityDetails.id.toString(), this.sta10Form.value).subscribe(
+                (data: any) => {
+                    console.log(data);
+                    this.step += 1;
+                    swal.fire({
+                        title: 'STA10 Form saved!',
+                        buttonsStyling: false,
+                        customClass: {
+                            confirmButton: 'btn btn-success form-wizard-next-btn ',
+                        },
+                        icon: 'success'
+                    });
+// this.router.navigate(['/users-list']);
+                },
+            );
+        }
+    }
 
 
 }
