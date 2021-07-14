a1cbe291d5557f5c69fac84efb72995fcf034f80
jmantu-bsk <james.mantu@bskglobaltech.com>
2021-07-14 13:34:04 +0300

    added permit ref number to QA task list when fetching tasks

--- apollo-api/src/main/kotlin/org/kebs/app/kotlin/apollo/api/ports/provided/bpmn/TaskDetails.kt
+++ apollo-api/src/main/kotlin/org/kebs/app/kotlin/apollo/api/ports/provided/bpmn/TaskDetails.kt
@@ -2,6 +2,6 @@ package org.kebs.app.kotlin.apollo.api.ports.provided.bpmn
 
 import org.flowable.task.api.Task
 
-class TaskDetails(var permitId: Long, var objectId:Long, var task: Task) {
+class TaskDetails(var permitId: Long, var objectId:Long, var task: Task, var permitRefNo:String) {
 
 }
