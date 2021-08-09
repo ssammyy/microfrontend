5cf95f864076f3b4ad6c625f18ece7eed18ce8ff
mungai <mungaij83@gmail.com>
2021-08-10 01:26:47 +0300

    AMM

---
+++ apollo-angular/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,158 @@
+import {Component, OnInit} from '@angular/core';
+import PerfectScrollbar from 'perfect-scrollbar';
+import {Store} from '@ngrx/store';
+import {loadLogout, selectUserInfo} from '../core/store';
+import {Router} from '@angular/router';
+
+declare const $: any;
+
+// Metadata
+export interface RouteInfo {
+    path: string;
+    title: string;
+    type: string;
+    icontype: string;
+    collapse?: string;
+    children?: ChildrenItems[];
+}
+
+export interface ChildrenItems {
+    path: string;
+    title: string;
+    ab: string;
+    type?: string;
+}
+
+// Menu Items
+export const ROUTES: RouteInfo[] = [
+    {
+        path: '/dashboard',
+        title: 'Dashboard',
+        type: 'link',
+        icontype: 'dashboard'
+    },
+
+    {
+        path: '/company',
+        title: 'My Companies',
+        type: 'sub',
+        icontype: 'business',
+        collapse: 'company',
+        children: [
+            {path: 'companies', title: 'View Companies', ab: 'VC'},
+            // {path: 'branches', title: 'View Branches ', ab: 'VB'},
+            {path: 'users', title: 'View Users ', ab: 'VU'}
+        ]
+    },
+
+    {
+        path: '/fmark',
+        title: 'Fortification Mark',
+        type: 'sub',
+        icontype: 'recommended',
+        collapse: 'fmark',
+        children: [
+            {path: 'application', title: 'Make Application', ab: 'MA'},
+            {path: 'fMarkAllApp', title: 'All My Applications', ab: 'AMA'},
+            {path: 'panels', title: 'Awarded Applications', ab: 'AA'}
+        ]
+    }, {
+        path: '/dmark',
+        title: 'Diamond Mark',
+        type: 'sub',
+        icontype: 'verified',
+        collapse: 'forms',
+        children: [
+            {path: 'newDmarkPermit', title: 'Make Application', ab: 'MA'},
+            {path: 'all_dmark', title: 'All My Applications', ab: 'AMA'},
+            {path: 'panels', title: 'Awarded Applications', ab: 'AA'}
+        ]
+    }, {
+        path: '/smark',
+        title: 'Standardization Mark',
+        type: 'sub',
+        icontype: 'class',
+        collapse: 'tables',
+        children: [
+            {path: 'newSmarkPermit', title: 'Make Application', ab: 'MA'},
+            {path: 'all_smark', title: 'All My Applications', ab: 'AMA'},
+            {path: 'panels', title: 'Awarded Applications', ab: 'AA'}
+        ]
+    }, {
+        path: '/invoice',
+        title: 'Invoices',
+        type: 'sub',
+        icontype: 'receipt',
+        collapse: 'invoice',
+        children: [
+            {path: 'all_invoice', title: 'All Invoices', ab: 'AI'},
+            {path: 'consolidate_invoice', title: 'Consolidate Invoices', ab: 'CI'}
+        ]
+    },
+    {
+        path: '/all_tasks_list',
+        title: 'My Tasks',
+        type: 'link',
+        icontype: 'task'
+    },
+    {
+        path: '/pvoc',
+        title: 'Import Inspection',
+        type: 'link',
+        icontype: 'receipt'
+    }
+];
+
+@Component({
+    selector: 'app-sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+
+export class SidebarComponent implements OnInit {
+    public menuItems: any[];
+    ps: any;
+    fullname = '';
+
+    constructor(
+        private store$: Store<any>, private router: Router
+    ) {
+    }
+
+    isMobileMenu() {
+        return $(window).width() <= 991;
+
+    }
+
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+            const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+            this.ps = new PerfectScrollbar(elemSidebar);
+        }
+        this.store$.select(selectUserInfo).pipe().subscribe((u) => {
+            return this.fullname = u.fullName;
+        });
+    }
+
+    updatePS(): void {
+        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+            this.ps.update();
+        }
+    }
+
+    isMac(): boolean {
+        let bool = false;
+        if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+            bool = true;
+        }
+        return bool;
+    }
+
+    onClickLogout() {
+        this.store$.dispatch(loadLogout({loginUrl: 'login'}));
+    }
+
+    onClickGoToProfilePage() {
+        this.router.navigate(['/profile']);
+    }
+}
