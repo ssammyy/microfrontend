aa2ca01af0dd3e229938c4f304448cbe5fa0bbfb
Stephen Muganda <stephenmuganda@gmail.com>
2021-08-13 11:20:40 +0300

    Move modules to subfolders

---
+++ apollo-angular-newui/src/app/apollowebs/standards-development/international-standard/international-standard-proposal/international-standard-proposal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-international-standard-proposal',
+  templateUrl: './international-standard-proposal.component.html',
+  styleUrls: ['./international-standard-proposal.component.css']
+})
+export class InternationalStandardProposalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apollo-angular-newui/src/app/apollowebs/standards-development/systemic-review/request-standard-review/request-standard-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-standard-review',
+  templateUrl: './request-standard-review.component.html',
+  styleUrls: ['./request-standard-review.component.css']
+})
+export class RequestStandardReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
