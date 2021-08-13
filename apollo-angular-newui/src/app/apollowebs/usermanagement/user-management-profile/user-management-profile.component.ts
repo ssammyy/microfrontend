a7bb1efe91393e398e8effdcad8ea3a6f40584fb
Kalenda Paul <kpaul7747@gmail.com>
2021-08-13 11:33:33 +0300

    USER DETAILS CONNECTED Signed-off-by: Kalenda Paul <kpaul7747@gmail.com>deta
    Signed-off-by: Kalenda Paul <kpaul7747@gmail.com>

--- apollo-angular-newui/src/app/apollowebs/usermanagement/user-management-profile/user-management-profile.component.ts
+++ apollo-angular-newui/src/app/apollowebs/usermanagement/user-management-profile/user-management-profile.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoadingService} from '../../../core/services/loader/loadingservice.service';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {UserEntityService} from '../../../core/store';
+import {MasterService} from '../../../core/store/data/master/master.service';
+import {UserRegister} from '../../../../../../apollo-webs/src/app/shared/models/user';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-user-management-profile',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManagementProfileComponent implements OnInit {
 
-  constructor() { }
+  public userID!: string;
+  public userDetails!: UserRegister;
 
-  ngOnInit(): void {
+  constructor(
+      private route: ActivatedRoute,
+      private _loading: LoadingService,
+      private SpinnerService: NgxSpinnerService,
+      private service: UserEntityService,
+      private masterService: MasterService,
+  ) {
   }
 
+  ngOnInit(): void {
+    this.getSelectedUser();
+  }
+
+  private getSelectedUser() {
+    this.route.fragment.subscribe(params => {
+      this.userID = params;
+      console.log(this.userID);
+      this.masterService.loadUserDetails(this.userID).subscribe(
+          (data: UserRegister) => {
+            this.userDetails = data;
+            // this.onSelectL1SubSubSection(this.userDetails?.employeeProfile?.l1SubSubSection);
+
+          }
+      );
+    });
+  }
 }
