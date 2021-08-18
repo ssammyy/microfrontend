ef1a61adff778762484ac11df9c5415fa5cf6851
Stephen Muganda <stephenmuganda@gmail.com>
2021-08-18 12:01:02 +0300

    Merge remote-tracking branch 'origin/staging-di-uat-v1' into staging-di-uat-v1

--- apollo-angular-newui/src/app/app.module.ts
+++ apollo-angular-newui/src/app/app.module.ts
@@ -126,11 +126,7 @@ import { StandardRequestComponent } from './apollowebs/standards-development/sta
 import { RequestStandardFormComponent } from './apollowebs/standards-development/standard-request/request-standard-form/request-standard-form.component';
 import { ReviewApplicationsComponent } from './apollowebs/standards-development/standard-request/review-applications/review-applications.component';
 import { StandardTaskComponent } from './apollowebs/standards-development/standard-request/standard-task/standard-task.component';
-import {ModalModule} from "ngb-modal";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-
-
-// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//import {ModalModule} from "ngb-modal";
 
 @NgModule({
     exports: [
@@ -169,8 +165,7 @@ import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
         MdModule,
         CommonModule,
         BrowserModule,
-        ModalModule,
-        NgbModule,
+        //ModalModule,
         // TODO: Discuss as it seems to kill sending of requests to backend
         // EffectsModule.forRoot([]),
         // EntityDataModule.forRoot(entityConfig),
