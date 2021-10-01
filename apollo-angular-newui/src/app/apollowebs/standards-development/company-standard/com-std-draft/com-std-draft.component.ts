e48819cf83fa57dc170e9c74d8c0311b52eedbc1
Stephen Muganda <stephenmuganda@gmail.com>
2021-10-01 11:11:32 +0300

    company standard process migrationon

---
+++ apollo-angular-newui/src/app/apollowebs/standards-development/company-standard/com-std-draft/com-std-draft.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+import {Subject} from "rxjs";
+import {ApproveDraft, ApproveSACJC, ComJcJustificationDec} from "../../../../core/store/data/std/std.model";
+import {StdComStandardService} from "../../../../core/store/data/std/std-com-standard.service";
+import {NgxSpinnerService} from "ngx-spinner";
+import {NotificationService} from "../../../../core/store/data/std/notification.service";
+import {HttpErrorResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'app-com-std-draft',
+  templateUrl: './com-std-draft.component.html',
+  styleUrls: ['./com-std-draft.component.css']
+})
+export class ComStdDraftComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+  tasks: ComJcJustificationDec[] = [];
+  public actionRequest: ComJcJustificationDec | undefined;
+  constructor(
+      private stdComStandardService:StdComStandardService,
+      private SpinnerService: NgxSpinnerService,
+      private notifyService : NotificationService
+  ) { }
+
+  ngOnInit(): void {
+    this.getJcSecTasks();
+  }
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
+  showToasterError(title:string,message:string){
+    this.notifyService.showError(message, title)
+  }
+  showToasterSuccess(title:string,message:string){
+    this.notifyService.showSuccess(message, title)
+
+  }
+  public getJcSecTasks(): void{
+    this.SpinnerService.show();
+    this.stdComStandardService.getJcSecTasks().subscribe(
+        (response: ComJcJustificationDec[])=> {
+          this.SpinnerService.hide();
+          this.dtTrigger.next();
+          this.tasks = response;
+        },
+        (error: HttpErrorResponse)=>{
+          this.SpinnerService.hide();
+          alert(error.message);
+        }
+    );
+  }
+  public onOpenModal(task: ComJcJustificationDec,mode:string): void{
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','modal');
+    if (mode==='approveJustification'){
+      this.actionRequest=task;
+      button.setAttribute('data-target','#approveJustification');
+    }
+    if (mode==='prepPd'){
+      this.actionRequest=task;
+      button.setAttribute('data-target','#prepPd');
+    }
+    if (mode==='approveDraft'){
+      this.actionRequest=task;
+      button.setAttribute('data-target','#approveDraft');
+    }
+
+    if (mode==='reject'){
+      this.actionRequest=task;
+      button.setAttribute('data-target','#reject');
+    }
+    // @ts-ignore
+    container.appendChild(button);
+    button.click();
+
+  }
+  public decisionOnAccept(approveDraft: ApproveDraft): void{
+    this.SpinnerService.show();
+    this.stdComStandardService.decisionOnDraft(approveDraft).subscribe(
+        (response: ComJcJustificationDec) => {
+          this.SpinnerService.hide();
+          this.showToasterSuccess('Success', `Justification Approved`);
+          console.log(response);
+          this.getJcSecTasks();
+        },
+        (error: HttpErrorResponse) => {
+          this.SpinnerService.hide();
+          this.showToasterError('Error', `Error Processing Action`);
+          console.log(error.message);
+          this.getJcSecTasks();
+          //alert(error.message);
+        }
+    );
+  }
+  public onDecisionReject(approveDraft: ApproveDraft): void{
+    this.SpinnerService.show();
+    this.stdComStandardService.decisionOnDraft(approveDraft).subscribe(
+        (response: ComJcJustificationDec) => {
+          this.SpinnerService.hide();
+          this.showToasterSuccess('Success', `Justification Rejected`);
+          console.log(response);
+          this.getJcSecTasks();
+        },
+        (error: HttpErrorResponse) => {
+          this.SpinnerService.hide();
+          this.showToasterError('Error', `Error Processing Action`);
+          console.log(error.message);
+          this.getJcSecTasks();
+          //alert(error.message);
+        }
+    );
+  }
+}
