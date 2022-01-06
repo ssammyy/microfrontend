d0b284dba80e47c90c49f59462e276fa3be945d2
SteveMuganda94 <stephen.muganda@bskglobaltech.com>
2022-01-06 13:53:21 +0300

    SLevy Process #

---
+++ apollo-angular-newui/src/app/apollowebs/standards-levy/standard-levy-site-visit-feedback/standard-levy-site-visit-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-standard-levy-site-visit-feedback',
+  templateUrl: './standard-levy-site-visit-feedback.component.html',
+  styleUrls: ['./standard-levy-site-visit-feedback.component.css']
+})
+export class StandardLevySiteVisitFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
