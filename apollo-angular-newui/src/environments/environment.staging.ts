4c39b41d33eb042769215b6f688ed79c285ad6cd
Marvin Makau <mbiko.mb@gmail.com>
2022-06-22 10:34:23 +0300

    -QA Prod Update

--- apollo-angular-newui/src/environments/environment.staging.ts
+++ apollo-angular-newui/src/environments/environment.staging.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
     https_enabled: true,
-    base_url: 'kimsint.kebs.org:8006'
+    // base_url: 'kimsint.kebs.org:8007',
+    base_url: `kimsfluxint.kebs.org:8007`,
+
 };
