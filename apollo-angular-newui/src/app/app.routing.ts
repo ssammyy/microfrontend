0e447e7a0df54273e209ddcb9ece55ffa976f257
marvoceo <marvoceo@gmail.com>
2022-10-19 08:00:51 +0300

    Merge fixes

--- apollo-angular-newui/src/app/app.routing.ts
+++ apollo-angular-newui/src/app/app.routing.ts
@@ -524,11 +524,9 @@ import {
 import {
     SamplesSubmittedComponent
 } from "./apollowebs/quality-assurance/reports/samples-submitted/samples-submitted.component";
-} from "./apollowebs/quality-assurance/reports/smark/permits-granted/permits-granted.component";
-import {StandardLevyRejectedChangesComponent} from "./apollowebs/standards-levy/standard-levy-rejected-changes/standard-levy-rejected-changes.component";
-import {ComplaintPlanBatchListComponent} from './apollowebs/market-surveillance/complainWorkPlan/complaint-plan-batch-list/complaint-plan-batch-list.component';
-import {ComplaintPlanListComponent} from './apollowebs/market-surveillance/complainWorkPlan/complaint-plan-list/complaint-plan-list.component';
-import {ComplaintPlanDetailsComponent} from './apollowebs/market-surveillance/complainWorkPlan/complaint-plan-details/complaint-plan-details.component';
+import {
+    StandardLevyRejectedChangesComponent
+} from "./apollowebs/standards-levy/standard-levy-rejected-changes/standard-levy-rejected-changes.component";
 
 export const routes: Routes = [
     {
